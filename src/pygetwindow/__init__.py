8992d5f6b936e426a958d422829a4d4e5793347e
Al Sweigart <asweigart@gmail.com>
2019-05-17 17:53:46 -0700

    Adjusting setup.py so it opens readme.md with the utf-8 encoding.

--- src/pygetwindow/__init__.py
+++ src/pygetwindow/__init__.py
@@ -19,7 +19,7 @@ Possible Future Features:
 get/click menu (win32: GetMenuItemCount, GetMenuItemInfo, GetMenuItemID, GetMenu, GetMenuItemRect)
 """
 
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 
 import sys
 import collections
